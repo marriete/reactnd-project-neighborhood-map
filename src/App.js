001f07272b3053adefed6895ae4278579434939d
Michael <arrietem@gmail.com>
2018-10-22 17:47:56 -0400

    Added default markers to Map component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ class App extends Component {
   state = {
     markers: [
       {title: "Dunkin' Donuts", position: {lat: 39.639102, lng: -84.225591}},
-      {title: "Chick-Fil-A", position: {lat: 39.629376, lng: -84.195062}}
+      {title: "Chick-Fil-A", position: {lat: 39.629376, lng: -84.195062}},
+      {title: "YMCA", position: {lat: 39.665685, lng: -84.240122}},
+      {title: "Kroger", position: {lat: 39.601971, lng: -84.230185}},
+      {title: "Meijer", position: {lat: 39.67104, lng: -84.220165}},
+      {title: "Walmart Supercenter", position: {lat: 39.630666, lng: -84.212289}},
+      {title: "Dayton Mall", position: {lat: 39.633682, lng: -84.220963}},
+      {title: "Rusty Bucket Tavern", position: {lat: 39.636468, lng: -84.221696}},
+      {title: "Rave Cinemas Dayton South and XD", position: {lat: 39.643232, lng: -84.228703}}
     ]
   }
 
