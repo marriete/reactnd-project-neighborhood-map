bda80601fb135fd758a3110c0589f1767bae02d6
Michael <arrietem@gmail.com>
2018-10-22 16:29:30 -0400

    Added dafault Map component to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Map from './Map.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Map
+          id="map"
+          options={{
+            center: {lat: 39.63867, lng: -84.215963},
+            zoom: 8
+          }}
+          onMapLoad={map => {
+            var marker = new window.google.maps.Marker({
+              position: {lat: 39.63867, lng: -84.215963},
+              map: map,
+              title: 'Hello Dayton!'
+            });
+          }} />
       </div>
     );
   }
---
+++ src/Map.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+	onScriptLoad() {
+		const map = new window.google.maps.Map(
+			document.getElementById(this.props.id),
+			this.props.options);
+		this.props.onMapLoad(map)
+	}
+
+	componentDidMount() {
+		if (!window.google) {
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA45Kaoio3gDd4K2qUppRAPv7qhKkzOh90`;
+			var x = document.getElementsByTagName('script')[0];
+			x.parentNode.insertBefore(script, x);
+
+			script.addEventListener('load', event => {
+				this.onScriptLoad()
+			})
+		} else {
+			this.onScriptLoad()
+		}
+	}
+
+	render() {
+		return (
+			<div style={{ width: 500, height: 500 }} id={this.props.id} ></div>
+		)
+	}
+}
+
+export default Map
