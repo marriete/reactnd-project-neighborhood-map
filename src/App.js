2e0edfab46959bb85754c932d4ce53a27d49e001
Michael <arrietem@gmail.com>
2018-10-25 14:59:04 -0400

    Added Yelp API calls for business data to be saved in state.markers

--- src/App.js
+++ src/App.js
@@ -5,19 +5,53 @@ import ListView from './ListView.js'
 import './App.css';
 import { Route, Link } from 'react-router-dom'
 
+
 class App extends Component {
   state = {
-    markers: [
-      {title: "Dunkin' Donuts", position: {lat: 39.639102, lng: -84.225591}},
-      {title: "Chick-Fil-A", position: {lat: 39.629376, lng: -84.195062}},
-      {title: "YMCA", position: {lat: 39.665685, lng: -84.240122}},
-      {title: "Kroger", position: {lat: 39.601971, lng: -84.230185}},
-      {title: "Meijer", position: {lat: 39.67104, lng: -84.220165}},
-      {title: "Walmart Supercenter", position: {lat: 39.630666, lng: -84.212289}},
-      {title: "Dayton Mall", position: {lat: 39.633682, lng: -84.220963}},
-      {title: "Rusty Bucket Tavern", position: {lat: 39.636468, lng: -84.221696}},
-      {title: "Rave Cinemas Dayton South and XD", position: {lat: 39.643232, lng: -84.228703}}
-    ]
+    locations: [
+      {title: "Dunkin' Donuts", position: {lat: 39.639102, lng: -84.225591}, id: "K1G-truG2BcJLhQMFGFa5A"},
+      {title: "Chick-Fil-A", position: {lat: 39.629376, lng: -84.195062}, id: "IvcTWxI_QbOx0zi4zISi_w"},
+      {title: "YMCA", position: {lat: 39.665685, lng: -84.240122}, id: "Nrei_iet_uUGferoEUyOIQ"},
+      {title: "Kroger", position: {lat: 39.601971, lng: -84.230185}, id: "XYsa7IbSTiASyfAudt5xVg"},
+      {title: "Meijer", position: {lat: 39.67104, lng: -84.220165}, id: "CXJxtvSZwB2s3RYueMVPaA"},
+      {title: "Walmart Supercenter", position: {lat: 39.630666, lng: -84.212289}, id: "QkhYR_uPqnQLsedHE96bhw"},
+      {title: "Dayton Mall", position: {lat: 39.633682, lng: -84.220963}, id: "qQn0TR-opTfiX02jJnbr0w"},
+      {title: "Rusty Bucket Tavern", position: {lat: 39.636468, lng: -84.221696}, id: "-2Bl3K5TtKXE3oknF_sdIw"},
+      {title: "Rave Cinemas Dayton South and XD", position: {lat: 39.643232, lng: -84.228703}, id: "0pFHC87CyM8KbHAuCC36sw"}
+    ],
+    markers: []
+  }
+
+  getMarkerData = (locations) => {
+    let markers = [];
+
+    locations.forEach((location) => {
+      let config = {
+        headers: {'Authorization': "Bearer 1rk0i7zyjBP9sW-Ji0lLaljcswABo0q42PQRS_lTSmR2t2eicmMnyI1Ux6_nL-djivA0n1YAlm-OoIzBKY9kzhwx5MGCZzm46Qk5wQIfA0BdoplrE_LWsFJs8IbPW3Yx"},
+        params: {
+          id: location.id
+        }
+      }
+
+      var proxyUrl = "http://localhost:8080/";
+      var targetUrl = `https://api.yelp.com/v3/businesses/${config.params.id}`;
+
+
+      fetch(proxyUrl + targetUrl, config)
+      .then(response => {
+        console.log("success")
+        response.json().then(data => {
+          console.log(data)
+          markers.push(data);
+          console.log(markers);
+        })
+      })
+      .catch(event => {
+        console.log("failure")
+      })
+    })
+
+    return markers;
   }
 
   createMarker = (map, mark) => {
@@ -26,6 +60,15 @@ class App extends Component {
       map: map,
       title: mark.title
     })
+  }
+
+  componentWillMount() {
+    let markers = this.getMarkerData(this.state.locations);
+    this.setState({markers});
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
   }
 
   render() {
@@ -40,12 +83,13 @@ class App extends Component {
                 zoom: 13
               }}
               createMarker={this.createMarker}
-              markers={this.state.markers} />
+              markers={this.state.locations} />
+            <Link className="to-list" to="/list">List View</Link>
             {/*<FilterMenu />*/}
           </section>
         )} />
         <Route path="/list" render={() => (
-          <ListView markers={this.state.markers} />
+          <ListView markers={this.state.locations} />
         )} />
       </div>
     );
--- src/ListView.js
+++ src/ListView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ListItem from './ListItem.js'
+import { Link } from 'react-router-dom'
 
 class ListView extends Component {
 	render() {
 		return (
 			<div>
+				<Link className="close-list" to="/">Close</Link>
 				{this.props.markers.map((marker, index) => (
 					<ListItem key={index} marker={marker} index={index} />
 				))}
