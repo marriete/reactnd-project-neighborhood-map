2a938b8f466e7d43066ee5ae169a0cf789acd74c
Michael <arrietem@gmail.com>
2018-10-30 14:00:07 -0400

    Altered formatting of ListItem components

--- src/ListItem.js
+++ src/ListItem.js
@@ -47,7 +47,7 @@ class ListItem extends Component {
 				</div>
 				<div className="list-item-info">
 					<span className="list-item-name">{index+1}. {marker.name}</span>
-					<div className="list-item-rating">{marker.rating}</div>
+					<span className="list-item-rating">{marker.rating}</span>
 					<span className="list-item-type">{marker.categories[0].title}</span>
 					<span className="list-item-phone">{marker.phone}</span>
 					<span className="list-item-address">{marker.location.address1}</span>
