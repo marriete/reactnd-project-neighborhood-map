0f194a3fd06f5bb9eca7a09f5fa89caca0118278
Michael <arrietem@gmail.com>
2018-10-31 13:23:44 -0400

    Removed console.log statement in InfoWindow.js

--- src/InfoWindow.js
+++ src/InfoWindow.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 class InfoWindow extends Component {
 	content() {
-		console.log(this.props.markerInfo)
 		if (this.props.markerInfo.hours) {
 			return (
 			`
